16b2f63205e585fcaa3c5de212df196f6d9c8675
Daurenbekuly <adaurenb@eub.kz>
2024-06-11 23:12:55 +0500

    refactoring 1.1

--- src/main/java/com/example/demo/config/CassandraConfig.java
+++ src/main/java/com/example/demo/config/CassandraConfig.java
@@ -1,19 +1,16 @@
 package com.example.demo.config;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 
 @Configuration
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
-    @NotNull
     @Override
     public String getKeyspaceName() {
         return "sashok";
     }
 
-    @NotNull
     @Override
     public String getContactPoints() {
         return "localhost";
